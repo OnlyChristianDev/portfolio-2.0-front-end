d6898f846caa381b92818c4560c22952dad81145
ChristianAtena <christiandeveloper123@gmail.com>
2025-04-21 12:19:23 -0300

    melhoria no design da página de projetos

--- src/views/projetos/ProjetosPage.vue
+++ src/views/projetos/ProjetosPage.vue
@@ -2,7 +2,21 @@
   <div class="main">
     <font-awesome-icon class="icon" :icon="faChevronLeft" />
     <div class="container-projetos">
-      <div class="projetos"></div>
+      <div class="projetos">
+        <div class="image-project"></div>
+        <div class="descricao-projeto">
+          <h1>Titulo</h1>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic architecto totam corrupti,
+            laboriosam consectetur explicabo pariatur laudantium fugiat veritatis eveniet quis ab,
+            expedita quibusdam maxime quo itaque maiores? Tenetur, obcaecati.
+          </p>
+            <button>
+              Confira
+              <font-awesome-icon class="icon-arrow" :icon="faArrowRight" />
+            </button>
+        </div>
+      </div>
       <div class="circles-components">
         <CircleComponent v-for="(project, index) in projects" :key="index" />
       </div>
@@ -12,7 +26,8 @@
 </template>
 
 <script setup>
-const projects = [1,2,4,5,6,7]
+const projects = [1, 2, 4, 5, 6, 7]
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
 import CircleComponent from './components/CircleComponent.vue'
 import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons'
 </script>
