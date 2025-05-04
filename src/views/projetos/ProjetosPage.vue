d1c35897663157be718050ff00bcd87f3dfdbec4
ChristianAtena <christiandeveloper123@gmail.com>
2025-05-04 12:59:41 -0300

    adição das informações dos projetos nos slides

--- src/views/projetos/ProjetosPage.vue
+++ src/views/projetos/ProjetosPage.vue
@@ -3,7 +3,6 @@
     <div @click="prevSlide">
       <font-awesome-icon class="icon" :icon="faChevronLeft" />
     </div>
-
     <div class="container-projetos">
       <Transition name="slide" mode="out-in">
         <div class="projetos" :key="projetoAtual">
@@ -20,7 +19,6 @@
           </div>
         </div>
       </Transition>
-
       <div class="circles-components">
         <CircleComponent
           v-for="(project, index) in projects"
@@ -37,33 +35,29 @@
   </div>
 </template>
 
-
 <script setup>
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
 import CircleComponent from './components/CircleComponent.vue'
 import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons'
 import { computed, ref } from 'vue'
+import projetos from './arrayProjetos'
 
 const projetoAtual = ref(0)
-const projects = [
-  { title: 'Título 1', description: '', image: '/img/1.jpg' },
-  { title: 'Título 2', description: 'Lorem ipsum…', image: '/img/2.jpg' },
-]
 
-const currentProject = computed(() => projects[projetoAtual.value])
+const currentProject = computed(() => projetos[projetoAtual.value])
 
 const nextSlide = () => {
-  projetoAtual.value = (projetoAtual.value + 1) % projects.length
+  projetoAtual.value = (projetoAtual.value + 1) % projetos.length
 }
 
 const prevSlide = () => {
-  projetoAtual.value =
-    (projetoAtual.value - 1 + projects.length) % projects.length
+  projetoAtual.value = (projetoAtual.value - 1 + projetos.length) % projetos.length
 }
 
 setInterval(() => {
-nextSlide()
-}, 5000);
+  nextSlide()
+}, 7000)
+
 </script>
 
 <style scoped src="../projetos/projetos.css"></style>
